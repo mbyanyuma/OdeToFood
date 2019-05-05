89c0dfde2c90f575d3b058f07eec133d6bb33149
Moses Byanyuma <mbyanyuma108@gmail.com>
2019-05-05 13:36:01 -0400

    Added in-memory data, models and model binding, edited data with razor pages

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,66 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
+        Restaurant Update(Restaurant updatedRestaurant);
+        int Commit();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        private readonly List<Restaurant> _restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            _restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Halal Pizza Cafe", Cuisine = CuisineType.Somalian, Location = "Columbus, OH" },
+                new Restaurant {Id = 2, Name = "Condado Tacos", Cuisine = CuisineType.Mexican, Location = "Columbus, OH"},
+                new Restaurant {Id = 3, Name = "Panera Bread", Cuisine = CuisineType.None, Location = "Columbus, OH"}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            //var restaurants = _restaurants.Where(x => x.Name.StartsWith(name) || !string.IsNullOrEmpty(name));
+
+            //return restaurants;
+
+            return from r in _restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
+                   orderby r.Name
+                   select r;
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return _restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = _restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+            if (restaurant != null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+
+            return restaurant;
+        }
+
+        public int Commit()
+        {
+            return 0;
+        }
+    }
+}
